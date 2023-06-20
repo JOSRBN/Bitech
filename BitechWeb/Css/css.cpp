2daa3ff11d8e7fb64590faf2f7a2c5d0f811e1aa
Brentec Neismart <progneosmart@gmail.com>
2023-06-20 10:05:47 -0600

    second push goga

---
+++ BitechWeb/Css/css.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Programacion  Brentec on 20/06/23.
+//
+
+#include "css.h"
---
+++ BitechWeb/Css/css.h
@@ -0,0 +1,16 @@
+//
+// Created by Programacion  Brentec on 20/06/23.
+//
+
+#ifndef BITECH_CSS_H
+#define BITECH_CSS_H
+
+
+
+class css {
+
+};
+
+
+
+#endif //BITECH_CSS_H
